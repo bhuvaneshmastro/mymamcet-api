d53813c014b903728caa44dc806b5ca2cb739ce7
unknown <bhuvanesh24gv@gmail.com>
2024-02-09 12:26:28 +0530

    add semester endpoints

--- v1/routers/semester.routes.js
+++ v1/routers/semester.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {add ,getsem} from "../controllers/semester.controller.js"
+import {add ,getsem, editSem} from "../controllers/semester.controller.js"
 const semesterRouter = express.Router()
 
 semesterRouter.route('/add').post(add)
 
-semesterRouter.route('/getSemester',).get( getsem)
+semesterRouter.route('/getSemester').get( getsem)
+
+semesterRouter.route('/editSemester').put(editSem)
 
 export { semesterRouter }
